4f4cd8e206a6a73aaab0d661be218b13f5377b9d
crisv <cristian.villanueva@passare.com>
2023-10-02 10:48:21 -0500

    shows updated

--- app/shows/page.js
+++ app/shows/page.js
@@ -4,26 +4,7 @@ import Show from "../components/show";
 function Shows() {
   return (
     <div className="flex flex-col items-center m-10 space-y-10">
-      <Show
-        date={"SEP 03"}
-        title={"Paper tiger show"}
-        location={"San antonio TX"}
-      />
-      <Show
-        date={"SEP 03"}
-        title={"Paper tiger show"}
-        location={"San antonio TX"}
-      />
-      <Show
-        date={"SEP 03"}
-        title={"Paper tiger show"}
-        location={"San antonio TX"}
-      />
-      <Show
-        date={"SEP 03"}
-        title={"Paper tiger show"}
-        location={"San antonio TX"}
-      />
+      <Show title={"No scheduled shows"} />
     </div>
   );
 }
