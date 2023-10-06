16b723d315a5a9f53cf71daa276d239a24fb0146
crisv <cristian.villanueva@passare.com>
2023-10-06 12:08:38 -0500

    Analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
           <Header />
         </div>
         {children}
+        <Analytics />
       </body>
     </html>
   );
