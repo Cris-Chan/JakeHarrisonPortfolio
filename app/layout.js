c30f7f1626c07369c96332f3e852eb85e02cf3f0
crisv <cristian.villanueva@passare.com>
2023-10-09 15:45:46 -0500

    sizing factors in mobile search bars (svh)

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} `}>
+      <body className={`${inter.className} box-border`}>
         <div style={{ position: "sticky", top: 0, zIndex: 1000 }}>
           <Header />
         </div>
